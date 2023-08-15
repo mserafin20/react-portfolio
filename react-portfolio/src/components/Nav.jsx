e7b3030dd02deb1e4f01322afd73626bc9363eb4
mserafin20 <mmserafin20@gmail.com>
2023-08-15 13:00:04 -0400

    filled in Nav.jsx with all the tabs that will appear in the navbar

--- react-portfolio/src/components/Nav.jsx
+++ react-portfolio/src/components/Nav.jsx
@@ -1,7 +1,45 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../styles/Nav.css';
+
 export default function Nav() {
+    const currentPage = useLocation().pathname;
+
+
     return (
-        <nav>
-            Nav
-        </nav>
+        <header>
+            <Navbar expand='lg' className='navbar'>
+                <Container>
+                    <Navbar.Brand className='my-name'>Michael Serafin</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto tabs'>
+                            <Link
+                                to='/'
+                                className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>
+                                About Me
+                            </Link>
+                            <Link
+                                to='/portfolio'
+                                className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}>
+                                Portfolio
+                            </Link>
+                            <Link
+                                to='/contact'
+                                className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}>
+                                Contact
+                            </Link>
+                            <Link
+                                to='/resume'
+                                className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}>
+                                Resume
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 };
